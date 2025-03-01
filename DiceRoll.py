04a6ad5547a8ac551888d2019ac387f156e48db7
lperafan <lperafan@unomaha.edu>
2025-03-01 04:46:56 +0000

    wordCount done

--- DiceRoll.py
+++ DiceRoll.py
@@ -8,11 +8,20 @@ def main():
   #Create an empty list with possible roll values
   rolls = [0,0,0,0,0,0,0,0,0,0,0,0]
   #Create two dice values ranging from 1 - 6 each
-  
+  for r in range(100):
+    dice1 = random.randint(1, 6)
+    dice2 = random.randint(1, 6)
+    sum_dice = dice1 + dice2
+    rolls[sum_dice - 1] += 1
+    print(f"Roll {r+1}: Dice 1 = {dice1}, Dice 2 = {dice2}, Sum = {sum_dice}")
+
   #find the sum total of the two dice
   
   #print statictics for dice rolls
-
+  dice = 1
+  for count in rolls:
+    print(dice, ":", count)
+    dice = dice + 1
 
 if __name__ == '__main__':
   main()
--- WordCount.py
+++ WordCount.py
@@ -4,10 +4,21 @@
 #Assignment:
 
 def main():
-  textFile = open("gettysberg.txt", 'r')
-  
+  textFile = open("fish.txt", 'r')
+  lineCount = 0
+  wordCount = 0
+  letterCount = 0
+
   for line in textFile:
-    print(line)
+    lineCount += 1
+    words = line.split()
+    for w in words:
+      wordCount += 1
+      letterCount += len(w)
+
+  print("Lines:", lineCount)
+  print("Words:", wordCount)
+  print("Letter:", letterCount)
   
 
 if __name__ == '__main__':
