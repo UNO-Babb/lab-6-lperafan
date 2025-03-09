fe8551bccdd9c5536f3a29d01317fd5bccc991eb
lperafan <lperafan@unomaha.edu>
2025-03-09 22:55:24 +0000

    done

--- WordIndex.py
+++ WordIndex.py
@@ -6,7 +6,7 @@
 def main():
   textFile = open("fish.txt", 'r')
   
-  words = {} #create an empty dictionary
+  words = {} 
   lineNum = 0
 
   for line in textFile:
@@ -23,10 +23,7 @@ def main():
       else:
         words[w] = [lineNum]
 
-  #print ("fish" in words) #is a word already in the dictionary?
-  #words["fish"] = [2]     #add a list to the dictionary
-  #print ("fish" in words) #is the word there now?
-  #words["fish"].append(5) #add to an existing list
+
   for word in words:
     print(word, words[word])
 
